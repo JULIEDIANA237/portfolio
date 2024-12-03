ea231e58b3c4af26a2b49a5919834dd60fdfbba4
juliefontsa237 <julie.fontsa@facsciences-uy1.cm>
2024-12-03 09:37:31 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Technologies from './components/Technologies';
 import Experience  from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
  
  
  const App = () => {
@@ -19,7 +21,9 @@ import Experience  from './components/Experience';
         <Hero />
         <About /> 
         <Technologies />
-        <Experience />
+        
+        <Projects />
+        <Contact />
       </div>
      </div>
    )
--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: " Yaoundé, Cameroun ",
+  phoneNo: "+237 693 32 63 99 ",
+  email: "dianajuliefontsa@gmail.com",
 };
